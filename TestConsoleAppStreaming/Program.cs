1b7e5cf29a7cf946ebb6a45b39622548c6b16545
Sverre Skodje <sskodje@gmail.com>
2022-05-09 02:15:01 +0200

    Added demo for streaming to RTMP server with FFMPEG.

---
+++ TestConsoleAppStreaming/Program.cs
@@ -0,0 +1,143 @@
+﻿using ScreenRecorderLib;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestConsoleAppStreaming
+{
+    class Program
+    {
+        private static bool _isRecording;
+        private static Stopwatch _stopWatch;
+        static void Main(string[] args)
+        {
+            var opts = new RecorderOptions
+            {
+                AudioOptions = new AudioOptions
+                {
+                    IsAudioEnabled = false,
+                },
+                VideoEncoderOptions = new VideoEncoderOptions
+                {
+                    IsFragmentedMp4Enabled = true,
+                    IsLowLatencyEnabled = true,
+                    Encoder = new H264VideoEncoder { BitrateMode = H264BitrateControlMode.Quality, EncoderProfile = H264Profile.Baseline },
+                    IsFixedFramerate = true,
+                    Framerate = 60
+                }
+            };
+            using (Recorder rec = Recorder.CreateRecorder(opts))
+            {
+                rec.OnRecordingFailed += Rec_OnRecordingFailed;
+                rec.OnRecordingComplete += Rec_OnRecordingComplete;
+                rec.OnStatusChanged += Rec_OnStatusChanged;
+                Console.WriteLine("Enter RTMP url and press ENTER to start recording");
+                string rtmpUrl = Console.ReadLine();
+
+                if (!rtmpUrl.StartsWith("rtmp://", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    Console.WriteLine("Invalid RTMP url. Press any key to exit.");
+                    Console.ReadKey();
+                    return;
+                }
+                var inputArgs = "-re -i -";
+                var outputArgs = $"-acodec copy -vcodec copy -f flv {rtmpUrl}";
+                var ffMpegProcess = new Process
+                {
+                    StartInfo =
+                        {
+                            FileName = "ffmpeg.exe",
+                            Arguments = $"{inputArgs} {outputArgs}",
+                            UseShellExecute = false,
+                            CreateNoWindow = true,
+                            RedirectStandardInput = true
+                        }
+                };
+                ffMpegProcess.Start();
+
+                using (Stream ffmpegIn = ffMpegProcess.StandardInput.BaseStream)
+                {
+                    using (StreamWrapper wrappedStream = new StreamWrapper(ffmpegIn))
+                    {
+                        rec.Record(wrappedStream);
+                        CancellationTokenSource cts = new CancellationTokenSource();
+                        var token = cts.Token;
+                        Task.Run(async () =>
+                        {
+                            while (true)
+                            {
+                                if (token.IsCancellationRequested)
+                                    return;
+                                if (_isRecording)
+                                {
+
+                                    Console.Write(String.Format("\rElapsed: {0}", _stopWatch.Elapsed.ToString(@"mm\:ss\:fff")));
+                                }
+                                await Task.Delay(10);
+                            }
+                        }, token);
+                        while (true)
+                        {
+                            ConsoleKeyInfo info = Console.ReadKey(true);
+                            if (info.Key == ConsoleKey.Escape)
+                            {
+                                break;
+                            }
+                        }
+                        cts.Cancel();
+                        rec.Stop();
+                        Console.WriteLine();
+                }
+                ffMpegProcess.WaitForExit();
+                Console.ReadKey();
+            }
+        }
+    }
+
+    private static void Rec_OnStatusChanged(object sender, RecordingStatusEventArgs e)
+    {
+        switch (e.Status)
+        {
+            case RecorderStatus.Idle:
+                break;
+            case RecorderStatus.Recording:
+                _stopWatch = new Stopwatch();
+                _stopWatch.Start();
+                _isRecording = true;
+                Console.WriteLine("Recording started");
+                Console.WriteLine("Press ESC to stop recording");
+                break;
+            case RecorderStatus.Paused:
+                Console.WriteLine("Recording paused");
+                break;
+            case RecorderStatus.Finishing:
+                Console.WriteLine("Finishing encoding");
+                break;
+            default:
+                break;
+        }
+    }
+
+    private static void Rec_OnRecordingComplete(object sender, RecordingCompleteEventArgs e)
+    {
+        Console.WriteLine("Recording completed");
+        _isRecording = false;
+        _stopWatch?.Stop();
+        Console.WriteLine(String.Format("File: {0}", e.FilePath));
+        Console.WriteLine();
+        Console.WriteLine("Press any key to exit");
+    }
+
+    private static void Rec_OnRecordingFailed(object sender, RecordingFailedEventArgs e)
+    {
+        Console.WriteLine("Recording failed with: " + e.Error);
+        _isRecording = false;
+        _stopWatch?.Stop();
+        Console.WriteLine();
+        Console.WriteLine("Press any key to exit");
+    }
+}
+}
---
+++ TestConsoleAppStreaming/StreamWrapper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleAppStreaming
+{
+    //The video encoder requires a stream that can seek, but it doesn't actually require seeking to work when encoding fragmented MP4.
+    //This stream wraps the non-seekable input stream from FFMPEG and lies about being able to seek, so the encoder is happy.
+    public class StreamWrapper : Stream
+    {
+        Stream _outStream;
+        public StreamWrapper(Stream outStream)
+        {
+            _outStream = outStream;
+        }
+
+        public override bool CanRead => false;
+
+        public override bool CanSeek => true;
+
+        public override bool CanWrite => true;
+
+        public override long Length => 0;
+
+        public override long Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void Flush()
+        {
+            _outStream.Flush();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            return offset;
+        }
+
+        public override void SetLength(long value)
+        {
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            try
+            {
+                _outStream.Write(buffer, offset, count);
+            }
+            catch (ObjectDisposedException) { }
+        }
+        public override void Close()
+        {
+            base.Close();
+            _outStream.Close();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            _outStream.Dispose();
+        }
+    }
+}
