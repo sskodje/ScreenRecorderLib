79d9bb349d4382e99c77a50f87b21c7c32dbb95f
Sverre Skodje <sskodje@gmail.com>
2024-09-27 11:29:58 +0200

    Fixed issue with mouse cursor rendering in bitmap callback

--- ScreenRecorderLibNative/MouseManager.cpp
+++ ScreenRecorderLibNative/MouseManager.cpp
@@ -96,8 +96,8 @@ HRESULT MouseManager::Initialize(_In_ ID3D11DeviceContext *pDeviceContext, _In_ 
 	BlendStateDesc.RenderTarget[0].SrcBlend = D3D11_BLEND_SRC_ALPHA;
 	BlendStateDesc.RenderTarget[0].DestBlend = D3D11_BLEND_INV_SRC_ALPHA;
 	BlendStateDesc.RenderTarget[0].BlendOp = D3D11_BLEND_OP_ADD;
-	BlendStateDesc.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND_ONE;
-	BlendStateDesc.RenderTarget[0].DestBlendAlpha = D3D11_BLEND_ZERO;
+	BlendStateDesc.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND_SRC_ALPHA;
+	BlendStateDesc.RenderTarget[0].DestBlendAlpha = D3D11_BLEND_INV_SRC_ALPHA;
 	BlendStateDesc.RenderTarget[0].BlendOpAlpha = D3D11_BLEND_OP_ADD;
 	BlendStateDesc.RenderTarget[0].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;
 	hr = pDevice->CreateBlendState(&BlendStateDesc, &m_BlendState);
